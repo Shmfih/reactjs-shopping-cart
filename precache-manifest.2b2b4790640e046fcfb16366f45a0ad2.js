b971caae15c5f4ab2515a167a181cc86c9d16a96
Kay <Shmfih@gmail.com>
2019-07-26 19:59:26 +0700

    Updates

---
+++ precache-manifest.2b2b4790640e046fcfb16366f45a0ad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e872c7c64c07228fc5304613feba3d47",
+    "url": "/reactjs-shopping-cart/index.html"
+  },
+  {
+    "revision": "dda594f4680a0a556eaa",
+    "url": "/reactjs-shopping-cart/static/css/main.1709142b.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/reactjs-shopping-cart/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "dda594f4680a0a556eaa",
+    "url": "/reactjs-shopping-cart/static/js/main.6584d564.chunk.js"
+  },
+  {
+    "revision": "816e03166a6316512da1",
+    "url": "/reactjs-shopping-cart/static/js/runtime~main.37803982.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-shopping-cart/static/media/logo.5d5d9eef.svg"
+  }
+]);
