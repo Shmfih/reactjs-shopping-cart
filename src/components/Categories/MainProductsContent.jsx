4560ee0dc6edf1302bfeac1a6668e9012a4daed2
Kay <Shmfih@gmail.com>
2019-08-26 22:17:48 +0700

    Fix bug

--- src/components/Categories/MainProductsContent.jsx
+++ src/components/Categories/MainProductsContent.jsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';np
+import PropTypes from 'prop-types';
 import ProductItem from './ProductItem/ProductItem';
 import { connect } from 'react-redux';
 import { addProductToCart } from '../../action/index';
